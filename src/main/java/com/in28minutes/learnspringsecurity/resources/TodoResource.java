7b0903b78c62c28bd03dbc6e7590432cbbfc0dbf
Sufiyan Shaikh <sufiwork2001@gmail.com>
2024-04-28 15:47:21 +0530

    first commit

---
+++ src/main/java/com/in28minutes/learnspringsecurity/resources/TodoResource.java
@@ -0,0 +1,32 @@
+package com.in28minutes.learnspringsecurity.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private static final List<Todo> TODO_LIST =
+            List.of(new Todo("in28minutes", "learn AWS"),
+                     new Todo("in28minutes", "Get AWS certified"));
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos(){
+        return TODO_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodosForSpecificUser(@PathVariable String username ){
+        return TODO_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodosForSpecificUser(@PathVariable String username ,@RequestBody Todo todo){
+        logger.info("Create {} for {}", todo,username);
+    }
+
+}
+record Todo(String username, String description) {}
